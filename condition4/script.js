529d18a71fe4456cd14f17bc06bb531a8ebe3fcf
jolieyolie <congyi.h.f@gmail.com>
2025-11-20 17:27:35 +0100

    fixed the bug on condition4

--- condition4/script.js
+++ condition4/script.js
@@ -1,22 +1,21 @@
-const slider = document.getElementById("AQIRange").value;
-const aqiOutput = document.getElementById("AQIOuput");
+const slider = document.getElementById("AQIRange");
+const aqiOutput = document.getElementById("AQIOutput");
 const aqiLevel = document.getElementById("AQIlevel");
-// updateUI();
-// slider.addEventListener('range', updateUI);
+updateUI();
+slider.addEventListener("input", updateUI);
 
 function updateUI() {
-  console.log(slider);
-  // const idx = Number(slider.value);
+  const idx = Number(slider.value);
   aqiOutput.innerText = slider;
-  // if (idx <= 50 && idx >= 0) {
-  //   aqiOutput.innerText =
-  //     "Level of health concern: Good Level of health effect: Little or no risk";
-  // } else if (idx <= 100 && idx >= 51) {
-  //   aqiOutput.innerText =
-  //     "Level of health concern: Moderate Level of health effect: Acceptable quality";
-  // } else if (idx <= 150 && idx >= 101) {
-  //   aqiOutput.innerText =
-  //     "Level of health concern: Unhealthy for sensitive groups Level of health effect: Generable publics not likely affected";
-  // }
+  if (idx <= 50 && idx >= 0) {
+    aqiOutput.innerText =
+      "Level of health concern: Good Level of health effect: Little or no risk";
+  } else if (idx <= 100 && idx >= 51) {
+    aqiOutput.innerText =
+      "Level of health concern: Moderate Level of health effect: Acceptable quality";
+  } else if (idx <= 150 && idx >= 101) {
+    aqiOutput.innerText =
+      "Level of health concern: Unhealthy for sensitive groups Level of health effect: Generable publics not likely affected";
+  }
   console.log(idx);
 }
