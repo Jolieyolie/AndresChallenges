afc722507a0840fe42abedc682d7c1e883727f5d
Yoli <yolanda.hu.es@gmail.com>
2026-01-04 23:27:31 +0100

    add text

--- ts-guessanumber/src/main.ts
+++ ts-guessanumber/src/main.ts
@@ -1,12 +1,3 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1 class="text-3xl font-bold underline text-blue-500">
-      Hello from TypeScript!
-    </h1>
-    <p class="mt-4 text-gray-600">
-      Tailwind CSS is working!
-    </p>
-  </div>
-`
+//document.querySelector<HTMLDivElement>('#app')!.innerHTML = 
